e12b8a04b57bdd61c1e95bc349d2845a83f99ab1
Lina Fang <april.azure@gmail.com>
2022-12-20 21:33:32 +0800

    day 20 part 2

--- src/Day20.java
+++ src/Day20.java
@@ -3,44 +3,49 @@ import java.util.List;
 
 public class Day20 {
 
-    int[] items;
+    long[] items;
     List<String> list = new ArrayList<>();
 
+    int M = 811589153;
+
     public void solve(List<String> lines) {
-        items = new int[lines.size()];
+        items = new long[lines.size()];
         int zeroIndex = 0;
         for (int i = 0; i < lines.size(); i++) {
-            int number = Integer.parseInt(lines.get(i));
+            long number = (long)Integer.parseInt(lines.get(i)) * M;
             items[i] = number;
             list.add(hash(i, number));
             if (number == 0) {
                 zeroIndex = i;
             }
         }
-
+        System.out.println(3 * M);
+        print(list);
         int MOD = lines.size() - 1;
 
-        for (int i = 0; i < items.length; i++) {
-            int pos = list.indexOf(hash(i, items[i]));
-            int step = items[i];
-            int newIndex = (pos + step) % MOD;
-            if (newIndex < 0) {
-                newIndex += MOD;
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < items.length; i++) {
+                int pos = list.indexOf(hash(i, items[i]));
+                long step = items[i];
+                int newIndex = (int)((pos + step) % MOD);
+                if (newIndex < 0) {
+                    newIndex += MOD;
+                }
+                list.remove(pos);
+                list.add(newIndex, hash(i, items[i]));
+
+
             }
-            list.remove(pos);
-            list.add(newIndex, hash(i, items[i]));
-
-//            print(list);
         }
-
+        print(list);
         findResult(list, zeroIndex);
     }
 
     private void findResult(List<String> list, int zeroIndex) {
         int pos = list.indexOf(hash(zeroIndex, 0));
-        int _1000th = Integer.parseInt(list.get((pos + 1000) % list.size()).split(",")[1]);
-        int _2000th = Integer.parseInt(list.get((pos + 2000) % list.size()).split(",")[1]);
-        int _3000th = Integer.parseInt(list.get((pos + 3000) % list.size()).split(",")[1]);
+        long _1000th = Long.parseLong(list.get((pos + 1000) % list.size()).split(",")[1]);
+        long _2000th = Long.parseLong(list.get((pos + 2000) % list.size()).split(",")[1]);
+        long _3000th = Long.parseLong(list.get((pos + 3000) % list.size()).split(",")[1]);
 
         System.out.println(_1000th + _2000th + _3000th);
     }
@@ -53,7 +58,7 @@ public class Day20 {
         System.out.println();
     }
 
-    private String hash(int i, int j) {
+    private String hash(int i, long j) {
         return i + "," + j;
     }
 }
