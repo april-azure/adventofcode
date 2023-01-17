0dcb6e3512f301605df07472cc62057a7f473fe6
Lina Fang <april.azure@gmail.com>
2023-01-17 16:53:47 +0800

    2020 day 16

--- src/AOC2020/Day202016.java
+++ src/AOC2020/Day202016.java
@@ -1,12 +1,16 @@
 package AOC2020;
 
+import Helper.Utils;
+
 import java.awt.image.AreaAveragingScaleFilter;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Day202016 {
 
     List<List<int[]>> ranges = new ArrayList<>();
+    List<int[]> tickets = new ArrayList<>();
+    Map<Integer, Integer> indexMap = new HashMap<>();
+    Map<Integer, Set<Integer>> invalids = new HashMap<>();
 
     public void solve(List<String> lines) {
         int lineForYourTicket = getLine(lines, "your ticket:");
@@ -21,7 +25,137 @@ public class Day202016 {
             ranges.add(range);
         }
 
+        for (int i = lineForNearbyTicket + 1; i < lines.size(); i++) {
+            int[] ticket = formatTicket(lines.get(i));
+            if (isValidTicket(ticket)) {
+                tickets.add(ticket);
+            }
+        }
 
+        dfs(0);
+        for (int key : indexMap.keySet()) {
+            System.out.println(key + " " + indexMap.get(key));
+        }
+
+        int[] yourTicket = formatTicket(lines.get(lineForYourTicket + 1));
+        long res = 1;
+        for (int i = 0; i < yourTicket.length; i++) {
+            int mappedIndex = indexMap.get(i);
+            if (mappedIndex < 6) {
+                res *= yourTicket[i];
+            }
+        }
+
+        System.out.println(res);
+    }
+
+    // two methods,
+    // 1) randomize the numbers, and check one by one
+    // 2) map first one and then recurisvely match all
+//    private void test1() {
+//        findAllMappings(new ArrayList<>(), 0);
+//    }
+
+    private boolean dfs(int numberIndex) {
+        if (numberIndex == ranges.size()) {
+            return true;
+        }
+
+        for (int i = 0; i < ranges.size(); i++) {
+            // check all valid indexes
+            if (mightBeValidRange(i, numberIndex)) {
+                indexMap.put(numberIndex, i);
+                if (dfs(numberIndex + 1)) {
+                    return true;
+                }
+                indexMap.remove(numberIndex);
+            }
+        }
+        return false;
+    }
+
+    private boolean mightBeValidRange(int i, int numberIndex) {
+        // if mapped
+        Set<Integer> mapped = new HashSet<>(indexMap.values());
+        if (mapped.contains(i)) {
+            return false;
+        }
+        // if is invalid
+        Set<Integer> invalidIndexes = invalids.getOrDefault(numberIndex, new HashSet<>());
+        if (invalidIndexes.contains(i)) {
+            return false;
+        }
+        List<int[]> targetRange = ranges.get(i);
+        for (int[] ticket : tickets) {
+            if (!isInRange(ticket[numberIndex], targetRange)) {
+                if (!invalids.containsKey(numberIndex)) {
+                    invalids.put(numberIndex, new HashSet<>());
+                }
+                invalids.get(numberIndex).add(i);
+                return false;
+            }
+        }
+        return true;
+    }
+
+//    private boolean testIsValidMapping(List<Integer> mapping) {
+//        for (int ticketIndex = 0; ticketIndex < tickets.size(); ticketIndex++) {
+//            for (int numberIndex = 0; numberIndex < ranges.size(); numberIndex++) {
+//                List<int[]> range = ranges.get(mapping.get(numberIndex));
+//                if (!isInRange(tickets.get(ticketIndex)[numberIndex], range)) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+//    }
+
+//    private boolean findAllMappings( List<Integer> path, int index) {
+//        if (path.size() == ranges.size()) {
+//            if (testIsValidMapping(path)) {
+//                Utils.print(path);
+//                return true;
+//            }
+//            return false;
+//        }
+//
+//        for (int i = 0; i <= path.size(); i++) {
+//            path.add(i, index);
+//            if (findAllMappings(path, index + 1)) {
+//                return true;
+//            }
+//            path.remove(i);
+//        }
+//        return false;
+//    }
+
+    private boolean isValidTicket(int[] ticket) {
+        for (int val : ticket) {
+            if (!isValidRange(val)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int[] formatTicket(String str) {
+        String[] parts = str.split(",");
+        int[] ticket = new int[parts.length];
+        for (int i = 0; i < ticket.length; i++) {
+            ticket[i] = Integer.parseInt(parts[i]);
+        }
+        return ticket;
+    }
+
+    private boolean isValidRange(int val) {
+        for (List<int[]> range : ranges) {
+            for (int[] r : range) {
+                if (val >= r[0] && val <= r[1]) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private boolean isInRange(int val, List<int[]> ranges) {
---
+++ src/Helper/Utils.java
@@ -0,0 +1,12 @@
+package Helper;
+
+import java.util.List;
+
+public class Utils {
+    public static void print(List<Integer> list) {
+        for (int val : list) {
+            System.out.print(val +" ");
+        }
+        System.out.println();
+    }
+}
